f846aa067c57e17aeb55575113abc811b312e390
Pankaj Chavhan <pankajchavhan5@gmail.com>
2022-10-02 18:38:56 +0530

    update unit test case

--- src/app/ui/navbar/header.component.spec.ts
+++ src/app/ui/navbar/header.component.spec.ts
@@ -1,11 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {BrowserModule} from '@angular/platform-browser';
-import {Router} from '@angular/router';
-import {RouterTestingModule} from '@angular/router/testing';
-import {AppRoutingModule} from 'src/app/app-routing.module';
-import {CartService} from 'src/app/service/cart.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from 'src/app/app-routing.module';
+import { CartService } from 'src/app/service/cart.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { HeaderComponent } from './header.component';
+import { RoutePaths } from 'src/app/enums/rout-path';
+import {of} from 'rxjs';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -13,27 +15,47 @@ describe('HeaderComponent', () => {
   let cartServiceSpy: jasmine.SpyObj<CartService>;
   let routerSpy: jasmine.SpyObj<Router>;
 
-  beforeEach(()=>{
-    cartServiceSpy = jasmine.createSpyObj('CartService',[
-      'getProducts','removeCartItems','removeAllcart','gettotalPrice'
+  const mockProducts: any = [
+    {
+      id: 1,
+      title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+      price: 109.95,
+      description:
+        'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+      category: "men's clothing",
+      image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+      rating: {
+        rate: 3.9,
+        count: 120,
+      },
+    },
+  ];
+
+  beforeEach(() => {
+    cartServiceSpy = jasmine.createSpyObj('CartService', [
+      'getProducts',
+      'removeCartItems',
+      'removeAllcart',
+      'gettotalPrice',
     ]);
     routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-  })
+    cartServiceSpy.getProducts.and.returnValue(of(mockProducts));
+  });
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ],
+      declarations: [HeaderComponent],
       imports: [
         RouterTestingModule,
         BrowserModule,
         AppRoutingModule,
         HttpClientTestingModule,
       ],
-      providers:[
+      providers: [
         { provide: Router, useValue: routerSpy },
-        { provide: cartServiceSpy, useValue: CartService}
-      ]
-    })
-    .compileComponents();
+        { provide: CartService, useValue: cartServiceSpy },
+      ],
+    }).compileComponents();
   });
 
   beforeEach(() => {
@@ -44,5 +66,27 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('#ngOnInit', () => {
+    it('should call getProducts', () => {
+      //Arrange
+
+      //Act
+      component.ngOnInit();
+      //Assert
+      expect(cartServiceSpy.getProducts).toHaveBeenCalled;
+    });
+  });
+
+  describe('#navigateToCart', () => {
+    it('should navigate to cart page when click cart button', () => {
+      //Arrange
+
+      //Act
+      component.navigateToCart();
+      //Assert
+      expect(routerSpy.navigate).toHaveBeenCalledWith([RoutePaths.CART]);
+    });
   });
 });
